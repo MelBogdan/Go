211b91598ff3136ba4f80f3f65a83d138a55f6e1
MelBogdan <maestro.25@mail.ru>
2021-03-05 11:59:46 +0300

    Задание к 5 уроку

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import "fmt"
+
+func main() {
+	var x int
+	fmt.Scan(&x)
+	x = fibb(x)
+	fmt.Println(x)
+}
+
+func fibb(n int) int {
+	m := map[int]int{}
+
+	num := 1
+
+	for i := 0; i <= n; i++ {
+		if i == 0 || i == 1 {
+			m[i] = num
+			continue
+		}
+		m[i] = m[i-2] + m[i-1]
+	}
+	return m[n]
+}
