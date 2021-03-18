281ba6c89dd174047218222e163998e5ec2a2db6
MelBogdan <maestro.25@mail.ru>
2021-03-18 18:37:56 +0300

    Задание к 9 уроку

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"flag"
-
 	"github.com/MelBogdan/Go/config"
 )
 
 func main() {
-	flag.Parse()
-
-	config.UrlValidation(*config.DB_url)
-	config.UrlValidation(*config.Jaeger_url)
-	config.UrlValidation(*config.Sentry_url)
+	config.Config()
 }
